6e4246ecc5e10f9073d8a10a8affd5f95c8641e0
rohanagarwal <rohan.agarwal204@gmail.com>
2022-11-19 12:57:13 -0500

    OpenFactory test so far

---
+++ test/erc721Factory/DealFactory.ts
@@ -0,0 +1,38 @@
+import { ethers } from "hardhat"
+import { expect } from "chai"
+import { arrayify } from "ethers/lib/utils"
+import { Contract } from "ethers"
+import { SignerWithAddress } from "hardhat-deploy-ethers/signers"
+import { Context, Setup } from "./../utils/Setup"
+import { SoulboundERC721 } from "../../typechain-types"
+
+
+describe("DealFactory", () => {
+    let context: Context
+
+    let NAME = "foo"
+    let SYMBOL = "bar"
+    let URI = "ipfs://hash"
+
+    beforeEach(async () => {
+        context = await Setup()
+    })
+
+    describe("create collection", () => {
+            it("should create a soulbound collection", async() => {
+                await context.openFactory.connect(context.users.user1).createCollection(NAME, SYMBOL, URI)
+
+                const collections = await context.soulboundStorage.getDeployedCollections(
+                    context.users.user1.address)
+                
+                const soulboundERC721 = (await ethers.getContractAt(
+                    "SoulboundERC721",
+                    collections[0]
+                )) as SoulboundERC721
+
+                expect(await soulboundERC721.name()).to.equal(NAME)
+                expect(await soulboundERC721.symbol()).to.equal(SYMBOL)
+                expect(await soulboundERC721.tokenURI(1)).to.equal(URI)
+            })
+    })
+})
